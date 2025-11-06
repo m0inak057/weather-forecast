29dc88a2b7a1460b4f769fa6da6d59d2fd7816f3
unknown <moinak.mondal057@gmail.com>
2025-11-07 00:39:51 +0530

    Fix WSGI and ASGI settings module path for deployment

--- weatherproject/weatherproject/wsgi.py
+++ weatherproject/weatherproject/wsgi.py
@@ -1,4 +1,4 @@
 import os
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weatherproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weatherproject.weatherproject.settings')
 application = get_wsgi_application()
